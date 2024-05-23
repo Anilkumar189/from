39920625eaaac0a92f8ab62770fc526843c6e1b0
Anilkumar189 <yandrapuanil143@gmail.com>
2024-05-23 04:37:30 -0400

    formik

---
+++ src/studentfrom.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as yup from 'yup';
+import axios from "axios";
+function Studentform(){
+    var Studentform=useFormik({
+        initialValues:{
+            firstname:"",
+            lastname:"",
+            age:"",
+            password:"",
+            username:""
+        },
+        // validationSchema:yup.object({
+        //     firstname:yup.string()
+        //     .required("please fill the firstname it is mandatory")
+        //     .max(5,"please check"),
+        //     lastname:yup.string()
+        //     .required("please fill the lastname")
+        //     .max(5,"please check"),
+        //     password:yup.string().matches(/^(?=.\d)(?=.[!@#$%^&])(?=.[a-z])(?=.*[A-Z]).{8,}$/,"please check"),
+        //     age:yup.number().test("checkage","age is not correct",(x)=>{
+        //        if(x>18 && x>=35){
+        //             return true
+        //         }
+        //         else{
+        //             return false
+        //         }
+        //     }),
+        //     username:yup.string().test("uniqueusername","user  name already in use",function(un){
+        //         var {path,createError}=this;
+        //         var p = new Promise((resolve ,reject)=>{
+        //         axios.get(`http://api.github.com/users/${un}`)
+        //         .then((res)=>{
+        //         reject(createError({path}))
+        //         })
+        //         .catch(err=>
+        //         {
+        //             resolve(true)
+        //         })
+        //     })
+        //        return p
+        //    })
+        // }),
+         onSubmit:(values)=>{
+            console.log(values);
+         }
+
+
+    })
+
+    return <div>
+        <form  onSubmit={Studentform.handleSubmit}>
+            <input type="text" {...Studentform.getFieldProps("firstname")} />
+            <div>
+                {Studentform.touched.firstname && Studentform.errors.firstname && <b>please fil firstname</b>}
+
+
+            </div>
+            <br/>
+            <input type="text" {...Studentform.getFieldProps("lastname")}  />
+            <br/>
+            <input type="text" {...Studentform.getFieldProps("age")}  /> 
+            <br/>
+            <input type="text" {...Studentform.getFieldProps("password")}  />
+            <br/>
+            <input type="text" {...Studentform.getFieldProps("username")}  />
+            <br/>
+            <button>show data</button>
+            <button onClick={()=>{Studentform.resetForm()}}>reset</button>
+            <button onClick={()=>{Studentform.setValues({ firstname:"anil kumar",
+            lastname:"yandrapu",
+            age:"24",
+            password:"123456",
+            username:"dady"})}}>show values</button>
+             <button onClick={()=>{Studentform.setFieldValue("lastname","dharamavarapu")}}>show lastname</button>
+        </form>
+        {JSON.stringify(Studentform.errors)}
+    </div>
+}
+export default Studentform
